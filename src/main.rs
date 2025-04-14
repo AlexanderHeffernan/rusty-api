b7043145518cb145b27bf4b695987719d686452d
Alexander Heffernan <heffernan.alexander@icloud.com>
2025-04-14 13:51:12 +1200

    Implementing rate limiting.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{App, HttpServer};
+use actix_governor::GovernorConfigBuilder;
+use actix_governor::Governor;
 use log::info;
 use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
 use crate::auth::Auth;
@@ -54,10 +56,17 @@ async fn main() -> std::io::Result<()> {
     let tls_config = load_rustls_config("certs/cert.pem", "certs/key.pem")
         .expect("Failed to load TLS configuration");
 
+    let governor_conf = GovernorConfigBuilder::default()
+        .per_second(3)
+        .burst_size(20)
+        .finish()
+        .unwrap();
+
     info!("Server running at https://127.0.0.1:8443");
     HttpServer::new(move || {
         App::new()
             .app_data(actix_web::web::Data::new(db_pool.clone()))
+            .wrap(Governor::new(&governor_conf))
             .wrap(Auth::new(db_pool.clone()))
             .service(routes::hello)
             .service(routes::admin)
